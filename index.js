73c0853b41df4c43dc49cfe860bbe28f3ba049d1
Rosemale-John II C. Villacorta <rosemalejohn@gmail.com>
2016-11-01 21:43:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname));
 
-app.listen(3000, function() {
+app.listen(app.get('port'), function() {
 	console.log('Connected!');
 });
