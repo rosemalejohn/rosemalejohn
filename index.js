966920a35c9e6aa3f4583c91b9b0ef516dafeacb
Rosemale-John II C. Villacorta <rosemalejohn@gmail.com>
2016-11-01 21:35:47 +0800

    Add node server configuration using express

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 var express = require('express');
 var app = express();
-var path = require('path');
 
-// viewed at http://localhost:3000
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-app.use('static', express.static('css'));
-app.use('static', express.static('img'));
-app.use('static', express.static('js'));
-app.use('static', express.static('vendor'));
+app.use(express.static(__dirname));
 
 app.listen(3000, function() {
 	console.log('Connected!');
