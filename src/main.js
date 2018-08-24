96f6caa8bc68728c172720d262d88e10fcfe2806
Rosemale-John II Villacorta <rosemalejohn@gmail.com>
2018-08-25 04:02:59 +0800

    Add meta properties with vue-head

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 
+// plugins
+import VueHead from 'vue-head'
+
 Vue.config.productionTip = false
+Vue.use(VueHead)
 
 new Vue({
   render: h => h(App)
