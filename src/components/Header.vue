9d15328d18b45e1e3ca3e0392df05d75733abddb
Rosemale-John II Villacorta <rosemalejohn@gmail.com>
2018-08-25 04:44:17 +0800

    Update site header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,10 +9,20 @@
           <hr>
         </div>
         <div class="col-lg-8 mx-auto">
-          <p class="text-faded mb-5">A passionate web developer from The Philippines</p>
+          <p class="text-faded mb-5">A passionate web developer from The Philippines | <a href="https://laravel.com" target="_blank" class="text-laravel">Laravel</a> and <a href="https://vuejs.org" target="_blank" class="text-vue">Vue</a> guru</p>
           <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Interested?</a>
         </div>
       </div>
     </div>
   </header>
 </template>
+
+<style scoped>
+  .text-laravel {
+    color: #FB503B;
+  }
+
+  .text-vue {
+    color: #4DBA87;
+  }
+</style>
