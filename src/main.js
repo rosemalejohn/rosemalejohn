8f69c282cb0354aae03c5a88fee4f5dbbd9f02b2
Rosemale-John II C. Villacorta <rosemalejohn@gmail.com>
2018-11-30 22:06:37 +0800

    update gitignore

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import App from './App.vue';
+import VueAnalytics from 'vue-analytics';
+import './registerServiceWorker';
+Vue.config.productionTip = false;
+// plugins
+Vue.use(VueAnalytics, {
+    id: 'UA-124624613-1',
+});
+new Vue({
+    render: (h) => h(App),
+}).$mount('#app');
+//# sourceMappingURL=main.js.map
