32bf2e2df05be24c969e193cbd0fcf46efa6696f
Rosemale-John II C. Villacorta <rosemalejohn@gmail.com>
2019-07-26 15:24:49 +0800

    Update Contact.vue

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,7 +3,7 @@
     <div class="container text-center">
       <h2 class="mb-4">Start a project</h2>
       <p>Interested in working together? That's great! Drop me an email and I will get back to you as soon as possible!</p>
-      <a @click="startProject" class="btn btn-light btn-xl sr-button" href="mailto:rosemalejohn@gmail.com?Subject=Project proposal">Let's do it!</a>
+      <a @click="startProject" class="btn btn-light btn-xl sr-button" href="mailto:contact@rosemalejohn.me?Subject=Project proposal">Let's do it!</a>
     </div>
   </section>
 </template>
